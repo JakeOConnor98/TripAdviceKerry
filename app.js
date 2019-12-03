8c119409274c877fbb8d2eefc180c2db8948de91
Jake O Connor <Jake.OConnor@students.ittralee.ie>
2019-12-03 15:10:47 +0000

    Added Commit

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 require('./app_api/models/db');
+require('./app_api/models/locations');
+require('./app_api/models/users');
+
 
 const index = require('./app_server/routes/index');
 const apiRoutes = require('./app_api/routes/index');
